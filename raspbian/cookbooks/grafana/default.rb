b6e8b8dde57854a27b6fa9e4ceb5f1e1df8d1b6e
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2020-08-21 19:53:11 -0300

    feat: create grafana recipe for raspberry pi v2

---
+++ raspbian/cookbooks/grafana/default.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+execute 'download grafana.deb package' do
+  command 'wget https://dl.grafana.com/oss/release/grafana-rpi_6.7.0_armhf.deb'
+end
+
+execute 'install grafana.deb' do
+  command 'dpkg -i grafana-rpi_6.7.0_armhf.deb'
+end
+
+execute 'enable grafana service' do
+  command 'systemctl enable grafana-server.service'
+end
+
+execute 'start grafana service' do
+  command 'systemctl start grafana-server.service'
+end
