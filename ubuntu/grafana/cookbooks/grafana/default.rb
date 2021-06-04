4687192f1ddb84daeef16661fd28218d344a4e78
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2021-06-04 10:18:54 -0300

    fix: more permission issues

--- ubuntu/grafana/cookbooks/grafana/default.rb
+++ ubuntu/grafana/cookbooks/grafana/default.rb
@@ -1,28 +1,28 @@
 
 # configure repositories
-execute 'wget -qO- https://repos.influxdata.com/influxdb.key | sudo gpg --dearmor > /etc/apt/trusted.gpg.d/influxdb.gpg'
-execute 'wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -'
+execute 'wget -qO- https://repos.influxdata.com/influxdb.key | apt-key add -'
+execute 'wget -qO- https://packages.grafana.com/gpg.key | apt-key add -'
 
-execute 'DISTRIB_ID="$(lsb_release -si)" DISTRIB_CODENAME="$(lsb_release -sc)" sudo echo "deb [signed-by=/etc/apt/trusted.gpg.d/influxdb.gpg] https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" > /etc/apt/sources.list.d/influxdb.list'
-execute 'sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"'
+execute 'echo "deb https://repos.influxdata.com/${"$(lsb_release -si)",,} $(lsb_release -sc) stable" > /etc/apt/sources.list.d/influxdb.list'
+execute 'add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"'
 
-execute 'sudo apt update -y'
+execute 'apt update -y'
 
 # install telegraf
-execute 'sudo apt install telegraf -y'
-execute 'sudo systemctl enable --now telegraf'
-execute 'sudo systemctl start telegraf'
+execute 'apt install telegraf -y'
+execute 'systemctl enable --now telegraf'
+execute 'systemctl start telegraf'
 
 # install influxdb
-execute 'sudo apt install influxdb -y'
-execute 'sudo systemctl enable --now influxdb'
-execute 'sudo systemctl start influxdb'
+execute 'apt install influxdb -y'
+execute 'systemctl enable --now influxdb'
+execute 'systemctl start influxdb'
 
 # install grafana
-execute 'sudo apt install grafana -y'
-execute 'sudo systemctl daemon-reload'
-execute 'sudo systemctl enable --now grafana-server'
-execute 'sudo systemctl start grafana-server'
+execute 'apt install grafana -y'
+execute 'systemctl daemon-reload'
+execute 'systemctl enable --now grafana-server'
+execute 'systemctl start grafana-server'
 
 
 # configure influxdb
@@ -37,7 +37,7 @@ execute 'sudo systemctl start grafana-server'
 
 
 # configure telegraf agent
-execute 'sudo telegraf config -input-filter cpu:mem:swap:system:processes:disk -output-filter influxdb > /etc/telegraf/telegraf.conf'
+execute 'telegraf config -input-filter cpu:mem:swap:system:processes:disk -output-filter influxdb > /etc/telegraf/telegraf.conf'
 
 # vim /etc/telegraf/telegraf.conf
 #
@@ -47,7 +47,7 @@ execute 'sudo telegraf config -input-filter cpu:mem:swap:system:processes:disk -
 # username = "telegraf"
 # password = "password"
 
-execute 'sudo systemctl restart telegraf'
+execute 'systemctl restart telegraf'
 
 execute 'telegraf --config /etc/telegraf/telegraf.conf --test'
 
