d2f7aa236bd8e2c1b2219f25765a5b1fdfa67871
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2021-06-04 16:18:29 -0300

    feat: telegrafdb creation and fix style

--- ubuntu/grafana/cookbooks/grafana/default.rb
+++ ubuntu/grafana/cookbooks/grafana/default.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
 
-system_name = `lsb_release -si | tr \'[:upper:]\' \'[:lower:]\'`
+system_name = %x(lsb_release -si | tr \'[:upper:]\' \'[:lower:]\')
 system_version = "bionic"
 
 # configure repositories
@@ -14,45 +15,40 @@ execute 'apt update -y'
 # install telegraf
 execute 'apt install telegraf -y'
 service 'telegraf' do
-  action [ :enable, :start ]
+  action [:enable, :start]
 end
 
 # install influxdb
 execute 'apt install influxdb -y'
 service 'influxdb' do
-  action [ :enable, :start ]
+  action [:enable, :start]
 end
 
 # install grafana
 execute 'apt install grafana -y'
 service 'grafana-server' do
-  action [ :enable, :start ]
+  action [:enable, :start]
 end
 
-
-# configure influxdb
-# influx
-#
-# > create database telegrafdb
-# > create user telegraf with password 'password'
-# > grant all on telegrafdb to telegraf
-# > show databases
-# > show users
-# > exit
-
+# configure influx telegrafdb
+execute 'influx -execute "create database telegrafdb"'
+execute 'influx -execute "create user telegraf with password \'password\'"'
+execute 'influx -execute "grant all on telegrafdb to telegraf"'
 
 # configure telegraf agent
 execute 'telegraf config -input-filter cpu:mem:swap:system:processes:disk -output-filter influxdb > /etc/telegraf/telegraf.conf'
 
 # vim /etc/telegraf/telegraf.conf
-#
+
 # [[outputs.influxdb]]
 # urls = ["http://127.0.0.1:8086"]
 # database = "telegrafdb"
 # username = "telegraf"
 # password = "password"
 
-execute 'systemctl restart telegraf'
+service 'telegraf' do
+  action [:restart]
+end
 
 execute 'telegraf --config /etc/telegraf/telegraf.conf --test'
 
