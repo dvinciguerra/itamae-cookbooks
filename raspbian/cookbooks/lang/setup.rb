442bee699e1f0d84253997edf7c21df5b3f59187
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2022-06-07 12:04:18 -0300

    feat: add lang cookbooks

---
+++ raspbian/cookbooks/lang/setup.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+execute "enable en_US locale.gen" do
+  command "perl -pi -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen"
+  not_if 'test -e "$(command -v perl)"'
+end
+
+execute "exec locale-gen" do
+  command "locale-gen en_US.UTF-8 && update-locale en_US.UTF-8"
+end
