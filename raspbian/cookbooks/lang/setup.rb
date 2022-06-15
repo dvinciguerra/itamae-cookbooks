d6ce5ed0bfd04a8b08207f4717b0df004a9c4118
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2022-06-14 21:16:05 -0300

    feat: change perl by sed to replace lang config

--- raspbian/cookbooks/lang/setup.rb
+++ raspbian/cookbooks/lang/setup.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 execute "enable en_US locale.gen" do
-  command "perl -pi -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen"
-  not_if 'test -e "$(command -v perl)"'
+  command "sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen"
+  not_if 'test "$LC_ALL" == "en_US.UTF-8"'
 end
 
 execute "exec locale-gen" do
   command "locale-gen en_US.UTF-8 && update-locale en_US.UTF-8"
+  not_if 'test ("$LC_ALL" == "en_US.UTF-8")'
 end
+
